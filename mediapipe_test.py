e2641b509b8a553c79886eaa5e68a52815a0c0b8
TheOnlyMann <82017462+TheOnlyMann@users.noreply.github.com>
2023-05-14 23:32:17 +0900

    CSV Export working, however need refinement

--- mediapipe_test.py
+++ mediapipe_test.py
@@ -1,102 +1,88 @@
-import datetime
 import cv2
 import mediapipe as mp
 import pandas as pd
 
-#stuff needed defined prior
-videoName = "C:/Users/ahn hyeontae/Videos/4K Video Downloader/Don't Say Goodbye - Line Dance (Demo & Walk Through).mp4"
+# Video file path
+video_path = "C:/Users/ahn hyeontae/Videos/4K Video Downloader/Don't Say Goodbye - Line Dance (Demo & Walk Through).mp4"
 pose_det_conf = 0.5
-post_trk_conf = 0.5
-# Mediapipe Pose Model
+pose_trk_conf = 0.5
+
+# Body keypoints name
+body_keypoints_name = {
+    0: "NOSE",
+    1: "NECK",
+    2: "RIGHT_SHOULDER",
+    3: "RIGHT_ELBOW",
+    4: "RIGHT_WRIST",
+    5: "LEFT_SHOULDER",
+    6: "LEFT_ELBOW",
+    7: "LEFT_WRIST",
+    8: "RIGHT_HIP",
+    9: "RIGHT_KNEE",
+    10: "RIGHT_ANKLE",
+    11: "LEFT_HIP",
+    12: "LEFT_KNEE",
+    13: "LEFT_ANKLE",
+    14: "RIGHT_EYE",
+    15: "LEFT_EYE",
+    16: "RIGHT_EAR",
+    17: "LEFT_EAR"
+}
+
+# Step 1: Set up the environment
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-#initialize pose model
-pose = mp_pose.Pose(min_detection_confidence = 0.5, min_tracking_confidence = 0.5)
-
-cap = cv2.VideoCapture(videoName)
-
+# Step 2: Initialize the video capture
+cap = cv2.VideoCapture(video_path)
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-body_keypoints_name = {
-            0: "NOSE",
-            1: "NECK",
-            2: "RIGHT_SHOULDER",
-            3: "RIGHT_ELBOW",
-            4: "RIGHT_WRIST",
-            5: "LEFT_SHOULDER",
-            6: "LEFT_ELBOW",
-            7: "LEFT_WRIST",
-            8: "RIGHT_HIP",
-            9: "RIGHT_KNEE",
-            10: "RIGHT_ANKLE",
-            11: "LEFT_HIP",
-            12: "LEFT_KNEE",
-            13: "LEFT_ANKLE",
-            14: "RIGHT_EYE",
-            15: "LEFT_EYE",
-            16: "RIGHT_EAR",
-            17: "LEFT_EAR"
-}
+# Step 3: Set up MediaPipe Pose detection
+with mp_pose.Pose(min_detection_confidence=pose_det_conf, min_tracking_confidence=pose_trk_conf) as pose:
+    pose_data = []
 
-def export_as_csv(obj): # Save per frame
+    # Step 4: Process video frames
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-    # Body ID, Date, and Time columns
-    now = datetime.datetime.now()
-    date_string = now.strftime("%Y-%m-%d")
-    time_string = now.strftime("%H:%M:%S.%f")
+        # Convert frame to RGB format
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    detection_results = {"Body_ID" : obj.id, "Date" : date_string, "Time" : time_string}
+        # Pass RGB frame to MediaPipe Pose detection
+        results = pose.process(frame_rgb)
 
-    # Keypoint columns
-    for i , keypnt in enumerate(obj.keypoint):
-        keypnt_str = ','.join(str(x) for x in keypnt)
-        detection_results[str(i)+"_"+body_keypoints_name[i]] = keypnt_str
+        if results.pose_landmarks:
+            # Retrieve pose landmarks
+            landmarks = results.pose_landmarks.landmark
 
-    # Convert the detection results to a DataFrame
-    df = pd.DataFrame(detection_results,index=[0])
-        
-    # Append the DataFrame to the CSV file
-    file_name = 'detection_results.csv'
-    with open(file_name, 'a', newline='') as f:
-        df.to_csv(f, header=f.tell()==0, index=False, mode='a', lineterminator="")               
-        
+            # Store the X, Y, and Z coordinates
+            landmark_data = [[landmark.x, landmark.y, landmark.z] for landmark in landmarks]
 
+            pose_data.append(landmark_data)
 
+        # Draw pose landmarks on the frame
+        mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                  landmark_drawing_spec=mp_drawing.DrawingSpec(color=(255, 0, 0)),
+                                  connection_drawing_spec=mp_drawing.DrawingSpec(color=(0, 255, 0)))
 
-#frame looping
-while True:
-    #1 frame 
-    ret, frame = cap.read()
-    if not ret:
-        break#end if no next frame
-    #convert color BGR->RGB
-    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # Display the resulting frame
+        cv2.imshow('Video Pose Detection', frame)
 
-    #result
-    result = pose.process(img)
-    mp_drawing.draw_landmarks(frame, result.pose_landmarks, mp_pose.POSE_CONNECTIONS,mp_drawing.DrawingSpec(color=(255,0,0)),mp_drawing.DrawingSpec(color=(0,255,0)))
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    new_h=height//2
-    new_w=width//2
-    resized_frame = cv2.resize(frame,(new_w,new_h))
-    cv2.imshow('Video', resized_frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # Create a pandas DataFrame from the pose data
+    df = pd.DataFrame(pose_data)
 
-#destroying all windows and freeing the used Video
-cap.release()
-cv2.destroyAllWindows()
+    # Save the DataFrame to a CSV file
+    df.to_csv('pose_landmarks.csv', index=False)
 
-
-
-
-
-
-
-
-
+    # Release the video capture and close windows
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 
