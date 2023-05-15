b60f2e1ecddf0743a781b0e7d7ad549d6969046b
TheOnlyMann <82017462+TheOnlyMann@users.noreply.github.com>
2023-05-16 01:39:22 +0900

    Update mediapipe_data_export.py

--- mediapipe/mediapipe_data_export.py
+++ mediapipe/mediapipe_data_export.py
@@ -8,22 +8,25 @@ mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
 # Step 2: Specify the directory containing the videos
-videos_directory = "path/to/videos"
+videos_directory = "C:\Users\ahn hyeontae\OneDrive - postech.ac.kr\POSTECH 자료\2023-01\CITE490K 패브리케이션\video"
 
-# Step 3: Set up MediaPipe Pose detection
+# Step 3: Specify the subdirectory for CSV files
+csv_directory = "C:\Users\ahn hyeontae\Documents\GitHub\CITE490K-Project\csv_extraction"
+
+# Step 4: Set up MediaPipe Pose detection
 with mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-    # Step 4: Iterate over the video files in the directory
+    # Step 5: Iterate over the video files in the directory
     for filename in os.listdir(videos_directory):
         if filename.endswith(".mp4"):
             video_path = os.path.join(videos_directory, filename)
 
-            # Step 5: Initialize video capture
+            # Step 6: Initialize video capture
             cap = cv2.VideoCapture(video_path)
 
-            # Step 6: Initialize pose data list
+            # Step 7: Initialize pose data list
             pose_data = []
 
-            # Step 7: Process video frames
+            # Step 8: Process video frames
             while cap.isOpened():
                 ret, frame = cap.read()
                 if not ret:
@@ -40,17 +43,14 @@ with mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.5, min_tra
                     landmarks = results.pose_landmarks.landmark
 
                     # Store the X, Y, and Z coordinates
-                    landmark_data = [landmark.x, landmark.y, landmark.z for landmark in landmarks]
+                    landmark_data = [[landmark.x, landmark.y, landmark.z] for landmark in landmarks]
 
                     pose_data.append(landmark_data)
 
                 # Draw pose landmarks on the frame
                 mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                          landmark_drawing_spec=mp_drawing.DrawingSpec(color=(255, 0, 0),
-                                                                                       thickness=2,
-                                                                                       circle_radius=2),
-                                          connection_drawing_spec=mp_drawing.DrawingSpec(color=(0, 255, 0),
-                                                                                          thickness=2))
+                                          landmark_drawing_spec=mp_drawing.DrawingSpec(color=(255, 0, 0)),
+                                          connection_drawing_spec=mp_drawing.DrawingSpec(color=(0, 255, 0)))
 
                 # Display the resulting frame
                 cv2.imshow('Video Pose Detection', frame)
@@ -58,18 +58,18 @@ with mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.5, min_tra
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
 
-            # Step 8: Export pose landmarks to CSV file
-            # Create a pandas DataFrame from the pose data
+            # Step 9: Create a pandas DataFrame from the pose data
             df = pd.DataFrame(pose_data)
 
-            # Create the output CSV file path
-            output_csv_path = os.path.splitext(video_path)[0] + ".csv"
+            # Step 10: Create the output CSV file path in the specified subdirectory
+            csv_filename = os.path.splitext(filename)[0] + ".csv"
+            csv_path = os.path.join(csv_directory, csv_filename)
 
-            # Save the DataFrame to a CSV file
-            df.to_csv(output_csv_path, index=False)
+            # Step 11: Save the DataFrame to a CSV file
+            df.to_csv(csv_path, index=False)
 
-            # Release the video capture
+            # Step 12: Release the video capture
             cap.release()
 
-    # Close all windows
+    # Step 13: Close all windows
     cv2.destroyAllWindows()
